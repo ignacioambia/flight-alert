7ca735dc976b97594d729b1d2e94c634aa1ed605
ignacio.ambia <ignacio.ambia@gmail.com>
2023-01-03 08:49:00 -0600

    fix: run tracking every hour

--- utils/tracker.js
+++ utils/tracker.js
@@ -1,7 +1,7 @@
 const cron = require("node-cron");
 const RouteTracker = require("../schemas/RouteTracker");
 
-cron.schedule("* */1 * * *", async () => {
+cron.schedule("0 */1 * * *", async () => {
   await new RouteTracker({}).save();
 });
 
